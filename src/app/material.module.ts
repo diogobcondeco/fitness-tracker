49fbf1204986a02c6537c4bc665b961bd5b5153d
Diogo Condêço <diogobcondeco@gmail.com>
2018-02-22 21:42:41 +0000

    Tab, Card, Dropdown
    Tab Component, Card and Dropdown Menu added

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -9,11 +9,42 @@ import {
   MatCheckboxModule,
   MatSidenavModule,
   MatToolbarModule,
-  MatListModule
+  MatListModule,
+  MatTabsModule,
+  MatCardModule,
+  MatSelectModule
 } from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatListModule],
-  exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatListModule]
+  imports: [
+    MatButtonModule, 
+    MatIconModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatDatepickerModule, 
+    MatNativeDateModule, 
+    MatCheckboxModule, 
+    MatSidenavModule, 
+    MatToolbarModule, 
+    MatListModule, 
+    MatTabsModule,
+    MatCardModule,
+    MatSelectModule
+  ],
+  exports: [
+    MatButtonModule, 
+    MatIconModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatDatepickerModule, 
+    MatNativeDateModule, 
+    MatCheckboxModule, 
+    MatSidenavModule, 
+    MatToolbarModule, 
+    MatListModule, 
+    MatTabsModule,
+    MatCardModule,
+    MatSelectModule
+  ]
 })
 export class MaterialModule { }
