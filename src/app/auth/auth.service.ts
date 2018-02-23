f51efe6b099a89bf0f155a1ebb0587d01f59d084
Diogo Condêço <diogobcondeco@gmail.com>
2018-02-23 02:24:47 +0000

    Routing & Authentication

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,18 +1,24 @@
 import { Subject } from 'rxjs/Subject';
-
+import { Router } from '@angular/router';
 import { User } from "./user.model";
 import { AuthData } from "./auth-data.model";
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class AuthService {
     authChange = new Subject<boolean>();
     private user: User;
+
+    constructor(private router: Router) {
+
+    }
 
     registerUser(authData: AuthData) {
         this.user = {
             email: authData.email,
             userId: Math.round(Math.random() * 10000).toString()
         };
-        this.authChange.next(true);
+        this.authSuccessfully();
     }
 
     login(authData: AuthData) {
@@ -20,12 +26,13 @@ export class AuthService {
             email: authData.email,
             userId: Math.round(Math.random() * 10000).toString()
         }
-        this.authChange.next(true);
+        this.authSuccessfully();
     }
 
     logout() {
         this.user = null;
         this.authChange.next(true);
+        this.router.navigate(['/login']);
     }
 
     getUser() {
@@ -34,5 +41,10 @@ export class AuthService {
 
     isAuth() {
         return this.user != null;
+    }
+
+    private authSuccessfully() {
+        this.authChange.next(true);
+        this.router.navigate(['/training']);
     }
 }
