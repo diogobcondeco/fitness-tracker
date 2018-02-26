5b36f3bf48f950195837457f30950f8f68b28d8a
Diogo Condêço <diogobcondeco@gmail.com>
2018-02-26 00:52:04 +0000

    Adding Real Authentication (Sign Up)

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,30 +3,33 @@ import { Router } from '@angular/router';
 import { User } from "./user.model";
 import { AuthData } from "./auth-data.model";
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from "angularfire2/auth";
 
 @Injectable()
 export class AuthService {
     authChange = new Subject<boolean>();
     private user: User;
 
-    constructor(private router: Router) {
+    constructor(private router: Router, private afAuth: AngularFireAuth) {
 
     }
 
     registerUser(authData: AuthData) {
-        this.user = {
-            email: authData.email,
-            userId: Math.round(Math.random() * 10000).toString()
-        };
-        this.authSuccessfully();
+        this.afAuth.auth.createUserWithEmailAndPassword(authData.email, authData.password).then(result => {
+            console.log(result);
+            this.authSuccessfully();
+        }).catch(error => {
+            console.log(error);
+        })
     }
 
     login(authData: AuthData) {
-        this.user = {
-            email: authData.email,
-            userId: Math.round(Math.random() * 10000).toString()
-        }
-        this.authSuccessfully();
+        this.afAuth.auth.signInWithEmailAndPassword(authData.email, authData.password).then(result => {
+            console.log(result);
+            this.authSuccessfully();
+        }).catch(error => {
+            console.log(error);
+        });
     }
 
     logout() {
