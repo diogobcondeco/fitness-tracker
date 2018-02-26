cb5caa07230547ca482489817a262b1f38200d70
Diogo Condêço <diogobcondeco@gmail.com>
2018-02-26 17:54:57 +0000

    Dispatching Actions & Selecting State Slices

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { AuthModule } from "./auth/auth.module";
 import { AngularFirestoreModule } from "angularfire2/firestore";
 import { TrainingModule } from "./training/training.module";
 import { StoreModule } from "@ngrx/store";
-import { appReducer } from "./app.reducer";
+import { reducers } from "./app.reducer";
 
 @NgModule({
   declarations: [
@@ -36,7 +36,7 @@ import { appReducer } from "./app.reducer";
     AuthModule,
     AngularFirestoreModule,
     TrainingModule,
-    StoreModule.forRoot({ ui: appReducer })
+    StoreModule.forRoot(reducers)
   ],
   providers: [AuthService, TrainingService, UIService],
   bootstrap: [AppComponent]
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -5,7 +5,7 @@ import { UIService } from "../../shared/ui.service";
 import { Subscription, Observable } from "rxjs";
 import { AuthModule } from "../auth.module";
 import { Store } from "@ngrx/store";
-import * as fromApp from "../../app.reducer";
+import * as fromRoot from "../../app.reducer";
 import { map } from "rxjs/operators";
 
 @Component({
@@ -21,11 +21,11 @@ export class LoginComponent implements OnInit {
   constructor(
     private authService: AuthService,
     private uiService: UIService,
-    private store: Store<{ ui: fromApp.State }>
+    private store: Store<fromRoot.State>
   ) {}
 
   ngOnInit() {
-    this.isLoading$ = this.store.map(state => state.ui.isLoading);
+    this.isLoading$ = this.store.select(fromRoot.getIsLoading);
     // this.loadingSubs = this.uiService.loadingStateChanged.subscribe(
     //   isLoading => {
     //     this.isLoading = isLoading;
