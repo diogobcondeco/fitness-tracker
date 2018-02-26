181c9836b0ad8820ec167f8674d6d0b54a75c9f7
Diogo Condêço <diogobcondeco@gmail.com>
2018-02-26 00:38:14 +0000

    Connecting the Data Table to Firestore

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -8,10 +8,9 @@ import { AngularFirestore } from 'angularfire2/firestore';
 export class TrainingService {
     exerciseChanged = new Subject<Exercise>();
     exercisesChanged = new Subject<Exercise[]>();
+    finishedExercisesChanged = new Subject<Exercise[]>();
     private availableExercises: Exercise[] = [];
-
     private runningExercise: Exercise;
-    private exercises: Exercise[] = [];
 
     constructor(private db: AngularFirestore) {}
 
@@ -58,8 +57,10 @@ export class TrainingService {
         return { ...this.runningExercise };
     }
 
-    getCompletedOrCancelledExercises() {
-        return this.exercises;
+    fetchCompletedOrCancelledExercises() {
+        this.db.collection('finishedExercises').valueChanges().subscribe((exercises: Exercise[]) => {
+            this.finishedExercisesChanged.next(exercises);
+        });
     }
 
     private addDataToDatabase (exercise: Exercise) {
