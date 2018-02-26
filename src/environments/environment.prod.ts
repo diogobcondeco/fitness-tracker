6afb0218d1edb282d5753a13086292e6129db1f0
Diogo Condêço <diogobcondeco@gmail.com>
2018-02-26 04:49:18 +0000

    Set Firebase for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC_9nha7-VGPV_WRkKRlscyvipZ-wcrmFM",
+    authDomain: "ng-fitness-tracker-4bcdb.firebaseapp.com",
+    databaseURL: "https://ng-fitness-tracker-4bcdb.firebaseio.com",
+    projectId: "ng-fitness-tracker-4bcdb",
+    storageBucket: "ng-fitness-tracker-4bcdb.appspot.com",
+    messagingSenderId: "232593831676"
+  }
 };
